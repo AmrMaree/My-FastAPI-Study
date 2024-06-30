09a90ef189380bb9752a1a987788619f85d4d034
AmrMaree <amr.maree14@gmail.com>
2024-06-30 17:03:31 +0200

    setting up the fastapi project

---
+++ apis.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(title="My FastAPI Project",description="This is my Project's API's Service")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins 
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def index():
+    return "I am alive"
