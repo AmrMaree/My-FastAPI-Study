013fa30b5221fa9d29ac242de1f5fd9a5d1dd1f9
AmrMaree <amr.maree14@gmail.com>
2024-07-06 05:16:57 +0200

    added the same functions but for mysql

--- services.py
+++ services.py
@@ -5,25 +5,28 @@ def get_messages():
     return data_access_sqlite.get_messages()
 
 def create_post(title : str, content : str, userid : int):
-    if data_access_sqlite.create_post(title , content , userid):
+    if data_access_mysql.create_post(title , content , userid):
         return {"message": "Created post successfully", "success": True}
     return {"message": "Failed to create post", "success": False}
     
 
 def create_comment(content : str, post_id : int, user_id : int):
-    if data_access_sqlite.create_comment(content, post_id, user_id):
+    if data_access_mysql.create_comment(content, post_id, user_id):
         return {"message": "Created comment successfully", "success": True}
     return {"message": "Failed to create comment", "success": False}
 
 def get_post_comments(post_id : int):
+    return data_access_mysql.get_post_comments(post_id)
     return data_access_sqlite.get_post_comments(post_id)
+    
 
 def delete_comment(id : int):
-    if data_access_sqlite.delete_comment(id):
+    if data_access_mysql.delete_comment(id):
         return {"message": "Deleted comment successfully", "success": True}
     return {"message": "Failed to delete comment", "success": False}
 
+
 def delete_post(id : int):
-    if data_access_sqlite.delete_post(id):
+    if data_access_mysql.delete_post(id):
         return {"message": "Deleted post successfully", "success": True}
     return {"message": "Failed to delete post", "success": False}
